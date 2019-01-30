daac5bdbb4dca68c88ab67aab15a29508c0a9e14
nisokaaa <www.nisoka@gmail.com>
2019-01-30 17:02:32 +0900

    CrystalShaderプロトタイプ完成

---
+++ Assets/Resources/Shaders/Box.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Box : MonoBehaviour {
+    [SerializeField]
+    bool Floating;
+    [SerializeField]
+    float FloatingSpeed;
+    [SerializeField]
+    float FloatDistance;
+    [SerializeField]
+    bool Rotate;
+    [SerializeField]
+    Vector3 RotSpeed;
+    [SerializeField]
+    bool sinX, sinY, sinZ;
+    
+    // Use this for initialization
+    void Start () {
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        if(Floating)
+        {
+            transform.position = new Vector3(
+                transform.position.x,
+                transform.position.y + Mathf.Sin(Time.time * FloatingSpeed) * FloatDistance,
+                transform.position.z);
+        }
+
+        if (Rotate)
+        {
+            Vector3 tSpeed = new Vector3();
+
+            tSpeed.x = sinX ? Mathf.Sin(Time.time * RotSpeed.x) : RotSpeed.x;
+            tSpeed.y = sinY ? Mathf.Sin(Time.time * RotSpeed.y) : RotSpeed.y;
+            tSpeed.z = sinZ ? Mathf.Sin(Time.time * RotSpeed.z) : RotSpeed.z;
+
+            transform.Rotate(tSpeed);
+        }
+        
+    }
+}
---
+++ Assets/Scripts/RotateCam.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RotateCam : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        // マウスの右クリックを押している間
+        if (Input.GetMouseButton(1))
+        {
+            // マウスの移動量
+            float mouseInputX = Input.GetAxis("Mouse X");
+            float mouseInputY = Input.GetAxis("Mouse Y");
+            // targetの位置のY軸を中心に、回転（公転）する
+            transform.RotateAround(transform.position, Vector3.up, mouseInputX * Time.deltaTime * 200f);
+            // カメラの垂直移動（※角度制限なし、必要が無ければコメントアウト）
+            transform.RotateAround(transform.position, transform.right, -mouseInputY * Time.deltaTime * 200f);
+        }
+    }
+}
